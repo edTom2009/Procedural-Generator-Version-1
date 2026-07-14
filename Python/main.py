c73990c479197548b97b89122c0abdddd8da0e1c
edTom2009 <edward.thomas.2009@icloud.com>
2026-07-15 00:57:59 +0200

    started on python project for the website, added login and create account pages, and dashboard page. Added css for login page. Added main.py file to run the website.

---
+++ Python/main.py
@@ -0,0 +1,19 @@
+#libraries
+import random
+import math
+import numpy
+
+
+#variables
+WIDTH = 100 #placeholder values
+HEIGHT = 100
+DETAIL = 50
+SEED = 0
+
+#functions
+def normalise(value, scale):
+    normValue = value/scale
+    return(normalValue)
+
+if SEED == 0:
+

9119224551cc011c2bc650e62a48b0f0a86953d8
edTom2009 <edward.thomas.2009@icloud.com>
2026-07-20 01:22:08 +0300

    added plan for python section

--- Python/main.py
+++ Python/main.py
@@ -15,5 +15,31 @@ def normalise(value, scale):
     normValue = value/scale
     return(normalValue)
 
-if SEED == 0:
+#GRADIENT VECTOR GENERATION
+#determine max size of gradient vector grid - based on map size and highest octave
+#generate a array of gradient vectors and calculate components
+
+
+#create array for based on map size
+#calculate the distance between each plotted pixdl and the next using the lacunarity value
+#add random offset for first pixel on grid
+#plot first pixel and calulate respecive distance vectors to each corner
+#take dot products of each distance and gradient vectors
+#interpolate between values first horizontally then vertically
+#store result as a single value in the array
+#normalise values to between 0 and 1
+#repeat for each pixel in map grid
+#repeat for each octave using respective lacunarity value
+#store each ocatve as its own array
+
+#divide each octave value by its persistance value to determine its eefect on the map
+#add octaves together for each pixel
+
+#render map
+#colour based on height value
+#water = 0 - 0.2
+#beach = 0.2 - 0.3
+#grass = 0.3 - 0.7
+#mountain = 0.7 - 0.9
+#snow = 0.9 - 1
 

f9ace0e0da83458177c8a0f9e8d704c3e2987540
Edward Thomas <edward.thomas.2009@icloud.com>
2026-08-13 14:25:13 +0000

    converted all lists to numpy arrays for easier manipulation and visualization

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,11 @@
 import random
 import math
 
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
 
 
 #variables
@@ -17,58 +22,70 @@ CELLSIZE = 20
 #GRADIENT VECTOR GENERATOR
 #generates a grid of gradient vectors for the map pixels to reference when calculating the dot product for each pixel
 def generateGradientVectors():
-    maxWidth = (octaves+1) * WIDTH // CELLSIZE
-    maxHeight = (octaves+1) * HEIGHT // CELLSIZE
-    gradVectGrid = []
+    maxWidth = (octaves + 1) * WIDTH // CELLSIZE
+    maxHeight = (octaves + 1) * HEIGHT // CELLSIZE
+    gradVectGrid = np.empty((maxHeight, maxWidth, 2), dtype=float)
 
-    for height in range (maxHeight):
-        gridRow = []
-        for width in range (maxWidth):
-            angleDeg = random.randint(0,360)
+    for y in range(maxHeight):
+        for x in range(maxWidth):
+            angleDeg = random.randint(0, 360)
             angleRad = math.radians(angleDeg)
-            i = math.sin(angleRad)
-            j = math.cos(angleRad)
-            coordValues = [i,j]
-            gridRow.append(coordValues)
+            gradVectGrid[y, x] = [math.sin(angleRad), math.cos(angleRad)]
 
-        gradVectGrid.append(gridRow)  
     return gradVectGrid
+
+def interpolateDotProducts(dot00, dot10, dot01, dot11, local_x, local_y):
+    grid = np.array([[dot00, dot10], [dot01, dot11]], dtype=float)
+
+    interpolator = sp.interpolate.RegularGridInterpolator(
+        (np.array([0.0, 1.0]), np.array([0.0, 1.0])),
+        grid,
+        method="linear"
+    )
+
+    return float(interpolator((local_x, local_y)))
+
 
 def generateOctaves(lacunarity, step, gradVectGrid):
     maxWidth = lacunarity * WIDTH
     maxHeight = lacunarity * HEIGHT
-    distVectGrid = []
-    y=0
-    for j in range (maxHeight):
-        gridRow = []
-        x=0
-        for i in range (maxWidth):
+    octaveGrid = np.empty((maxHeight, maxWidth), dtype=float)
+
+    y = 0
+    for j in range(maxHeight):
+        x = 0
+        for i in range(maxWidth):
             gridX = x // CELLSIZE
             gridY = y // CELLSIZE
 
-            v00 = gradVectGrid[gridY][gridX]         # top left
-            v10 = gradVectGrid[gridY][gridX + 1]     # top right
-            v01 = gradVectGrid[gridY + 1][gridX]     # bottom left
-            v11 = gradVectGrid[gridY + 1][gridX + 1] # bottom right
+            v00 = gradVectGrid[gridY, gridX]         # top left
+            v10 = gradVectGrid[gridY, gridX + 1]     # top right
+            v01 = gradVectGrid[gridY + 1, gridX]     # bottom left
+            v11 = gradVectGrid[gridY + 1, gridX + 1] # bottom right
 
             local_x = (x % CELLSIZE) / CELLSIZE
             local_y = (y % CELLSIZE) / CELLSIZE
 
-            d00 = [local_x, local_y]
-            d10 = [local_x - 1, local_y]
-            d01 = [local_x, local_y - 1]
-            d11 = [local_x - 1, local_y - 1]
+            d00 = np.array([local_x, local_y], dtype=float)
+            d10 = np.array([local_x - 1, local_y], dtype=float)
+            d01 = np.array([local_x, local_y - 1], dtype=float)
+            d11 = np.array([local_x - 1, local_y - 1], dtype=float)
 
             dot00 = v00[0] * d00[0] + v00[1] * d00[1]
             dot10 = v10[0] * d10[0] + v10[1] * d10[1]
             dot01 = v01[0] * d01[0] + v01[1] * d01[1]
             dot11 = v11[0] * d11[0] + v11[1] * d11[1]
 
-            x=x+step
+            interpolated_value = interpolateDotProducts(
+                dot00, dot10, dot01, dot11, local_x, local_y
+            )
+            octaveGrid[j, i] = interpolated_value
 
-        distVectGrid.append(gridRow)
-    
-    return distVectGrid
+            x += step
+
+        y += step
+
+    return octaveGrid
     
 
 gradVectGrid = generateGradientVectors()
@@ -76,6 +93,10 @@ octave1 = generateOctaves(1, 3, gradVectGrid)
 #print(octave1)
 print(len(octave1))
 print(len(octave1[0]))
+
+fig, ax = plt.subplots()
+ax.imshow(octave1, cmap='gray')
+plt.show()
 #print(len(octave1[0]))
 
 #create array for based on map size
